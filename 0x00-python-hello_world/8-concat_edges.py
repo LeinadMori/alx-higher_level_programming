5159c4d247c611ceffcbc097f5a7b6c345adeedb
LeinadMori <iromdaniel@gmail.com>
2023-10-02 21:17:00 -0700

    Python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "object-oriented programming with Python")
-print("\n".join(str.split()))
-
+str = "object-oriented programming with Python"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(str[str.find("object"):str.find("object") + 33] + "\n")
