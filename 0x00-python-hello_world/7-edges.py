254f003f1b2ce51b3579f6b5e113f020dc908ace
LeinadMori <iromdaniel@gmail.com>
2023-10-03 06:55:37 -0700

    7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-first_three_letters
-last_two_letters = word[-2:]
-removed_letters = word[1:-1]
-
-print(f"First 3 letters: {first_three_letters}")
-print(f"Last 2 letters: {last_two_letters}")
-print(f"Without first and last letter: {removed_letters}")
+word_first_3 = word[:3]
+word_last_2 = word[-2]
+middle_word = word[1:1]
+print ("First 3 letter: {}".format(word_first_3)
+print ("Last 2 letter: {}".format(word_last_2)
+print ("Middle word: {}".format(middle_word)
