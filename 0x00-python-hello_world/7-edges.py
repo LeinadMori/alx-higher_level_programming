55bed203995177554d2e62ca2d10d54f6ecda4fd
LeinadMori <iromdaniel@gmail.com>
2023-10-03 06:25:24 -0700

    7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
-
-# Get the first 3 letters
-first_three_letters = word[:3]
-
-# Get the last 2 letters
+first_three_letters
 last_two_letters = word[-2:]
-
-# Remove the first and last letter
 removed_letters = word[1:-1]
 
 print(f"First 3 letters: {first_three_letters}")
