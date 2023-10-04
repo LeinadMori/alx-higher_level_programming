4bdf6d3a451fda4795d4a1bbc1ea9fde2532f6d7
LeinadMori <iromdaniel@gmail.com>
2023-10-04 08:22:31 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
-#!/usr/bin/python3
+# Initialize an empty string to store the alphabet
+alphabet = ""
 
+# Loop through each character in the alphabet and add it to the string if it's not 'e' or 'q'
 for letter in 'abcdefghijklmnopqrstuvwxyz':
     if letter != 'e' and letter != 'q':
-        print(letter, end='')
+        alphabet += letter
 
-
+# Use a single print statement to print the entire alphabet without newlines
+print(f"{alphabet}")
