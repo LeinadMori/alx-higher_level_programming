15216893870d2f45be01024e93faa0fb0d0be701
LeinadMori <iromdaniel@gmail.com>
2023-10-05 10:12:06 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Import functions from calculator_1.py
+from calculator_1 import add, sub, mul, div
+
+# Define the variables a and b
+a = 10
+b = 5
+
+# Perform calculations and print the results
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
