8a869363cbb9180d0063ac4cdd04e8a911245e12
LeinadMori <iromdaniel@gmail.com>
2023-10-04 18:16:13 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-# Initialize an empty string to store the alphabet
 alphabet = ""
-
 for letter in 'abcdefghijklmnopqrstuvwxyz':
     if letter != 'e' and letter != 'q':
         alphabet += letter
-print("{}".format(alphabet))
+print("{}".format(alphabet), end="")
