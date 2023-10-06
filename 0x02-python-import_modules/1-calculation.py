238893d6a56c7b82f79c0c4977a4ab08c329cbdb
LeinadMori <iromdaniel@gmail.com>
2023-10-05 19:45:06 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,11 +4,15 @@
 from calculator_1 import add, sub, mul, div
 
 # Define the variables a and b
-a = 10
-b = 5
+def funxtionname():
+	a = 10
+	b = 5
 
-# Perform calculations and print the results
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+	# Perform calculations and print the results
+	print("{} + {} = {}".format(a, b, add(a, b)))
+	print("{} - {} = {}".format(a, b, sub(a, b)))
+	print("{} * {} = {}".format(a, b, mul(a, b)))
+	print("{} / {} = {}".format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    funxtionname()
