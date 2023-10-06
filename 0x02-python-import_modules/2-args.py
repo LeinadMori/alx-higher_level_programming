16dcf7515fe0e0b85e9c7e15d4c990e5d5998be4
LeinadMori <iromdaniel@gmail.com>
2023-10-05 21:09:17 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    # Retrieve the command-line arguments
+    argv = sys.argv[1:]
+
+    # Calculate the number of arguments
+    num_arguments = len(argv)
+
+    # Define a string that contains the singular or plural form of "argument"
+    plural_suffix = 's' if num_arguments != 1 else ''
+    # Create the message and print it
+    message = "{} argument{}:".format(num_arguments, plural_suffix)
+    print(message)
