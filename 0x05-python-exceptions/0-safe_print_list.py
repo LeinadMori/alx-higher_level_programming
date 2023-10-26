6185930f4561560ec7d5721877bb95dabd2c7a8d
LeinadMori <iromdaniel@gmail.com>
2023-10-25 19:32:55 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end=" ")  # Print each element on the same line
-            count += 1  # Increment the count
-    except IndexError:
-        pass  # If an IndexError occurs, just continue
-
-    print()  # Print a new line
-    return count  # Return the real number of elements printed
-
-# Example usage:
-my_list = [1, 2, 3, 4, 5]
-x = 8
-real_count = safe_print_list(my_list, x)
-print(f"Real number of elements printed: {real_count}")
+    num = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            num += 1
+        except IndexError:
+            break
+    print("")
+    Return
