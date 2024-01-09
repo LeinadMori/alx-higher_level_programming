d07a818727331ae7c957f5ebc971d93061ec6f3f
LeinadMori <iromdaniel@gmail.com>
2024-01-08 16:01:30 -0800

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function nPrint (n, txt) {
+  if (Number.isNaN(n)) {
+    console.log('Missing number of occurrences');
+  } else if (n >= 0) {
+    for (let i = 0; i < n; i++) {
+      console.log(txt);
+    }
+  }
+}
+
+nPrint(Number.parseInt(process.argv[2]), 'C is fun');
