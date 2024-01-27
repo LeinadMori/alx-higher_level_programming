ab40790db3df81c503c35f6de9351caccc966d63
LeinadMori <iromdaniel@gmail.com>
2024-01-27 04:21:19 -0800

    6-post_email.py 3-error_code.py 4-hbtn_status.py 5-hbtn_header.py5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""script that takes in a URL and an email address, sends a POST request
-to the passed URL with the email as a parameter, and finally displays
-the body of the response.
 """
+ takes in a URL, sends a request to the URL and displays
+ the value of the variable X-Request-Id in the response header
+"""
+
 import requests
 import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    email = sys.argv[2]
-    r = requests.post(url, data={'email': email})
-    print(r.text)
+
+    r = requests.get(url)
+    print(r.headers.get('X-Request-Id'))
