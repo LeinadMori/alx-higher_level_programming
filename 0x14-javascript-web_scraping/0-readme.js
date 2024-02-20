95a01a98848cad91b6437f094912554b47ec365e
LeinadMori <iromdaniel@gmail.com>
2024-02-20 05:13:43 -0800

    fixing the code

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const args = process.argv;
 const fs = require('fs');
 fs.readFile(args[2], 'utf8', (err, data) => {
-        if (err) {
-                console.log(err);
-                return;
-        }
-        console.log(data);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });
