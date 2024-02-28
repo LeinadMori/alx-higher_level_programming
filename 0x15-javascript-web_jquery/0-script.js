2db900c64252bb212319a3292be98fc8c6aeccd2
LeinadMori <iromdaniel@gmail.com>
2024-02-28 11:50:41 +0000

    **Subject: Update header color to red using JavaScript**
    This commit updates the color of the header element to red (#FF0000) using JavaScript.

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,23 @@
+CTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Header Color</title>
+</head>
+<body>
+
+    <header>
+        <h1>This is the header</h1>
+    </header>
+
+    <script>
+        // Select the <header> element using document.querySelector
+        const headerElement = document.querySelector('header');
+
+        // Update the text color to red (#FF0000)
+        headerElement.style.color = '#FF0000';
+    </script>
+
+</body>
+</html>
