c4183442b017f0e4dc1e5d3c0f6a5cb3d272214e
LeinadMori <iromdaniel@gmail.com>
2024-02-29 02:52:53 -0800

    rewrte the code in a js

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,27 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Change Header Color on Click</title>
-    <!-- Include jQuery from a CDN -->
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-</head>
-<body>
-
-    <div id="red_header">Click me to change header color</div>
-
-    <header>
-        <h1>This is the header</h1>
-    </header>
-
-    <script>
-        // Use jQuery to handle click event on DIV#red_header
-        $('#red_header').click(function() {
-            // Update the text color of the <header> element to red (#FF0000)
-            $('header').css('color', '#FF0000');
-        });
-    </script>
-
-</body>
-</html>
+// Use jQuery to handle click event on DIV#red_header
+$('#red_header').click(function() {
+	// Update the text color of the <header> element to red (#FF0000)
+            $('header').css('color', '#FF0000'); 
+});
